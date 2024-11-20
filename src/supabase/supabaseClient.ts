f1b0a8d7d03513f03e75c5cc94f75be816c5b961
Rahul <drlevio123@gmail.com>
2024-11-20 17:20:59 +0530

    commit

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl:string=process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey:string=process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl:string="https://asfabyswqpycwvvphguu.supabase.co"!;
+const supabaseAnonKey:string="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZmFieXN3cXB5Y3d2dnBoZ3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwMjY1NTgsImV4cCI6MjA0NzYwMjU1OH0.O-yDPBaNnPJJCvoMX87b1BkIkOZsI8RDUNxw77UboTY"!;
 
 export const supabase=createClient(supabaseUrl,supabaseAnonKey);
